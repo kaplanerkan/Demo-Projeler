d5d65272e2f858369d58463ad70322c8aba6d906
Erkan Kaplan <Kaplan.erkan@gmail.com>
2024-06-29 17:58:38 +0200

    firstinit

---
+++ 02-BottomSheetAndroid/app/src/main/java/com/lotuspecas/a02_bottomsheetandroid/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.lotuspecas.a02_bottomsheetandroid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initviews()
+    }
+
+    private fun initviews() {
+        val bottomSheet = BottomSheetFragment()
+        bottomSheet.show(supportFragmentManager, "BottomSheetFragment")
+    }
+
+
+}
